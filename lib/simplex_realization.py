f40cfb3bdfb6f1b36dbc1b78627389590e3e6fed
bogdanduminyuk <77934329bogdan@gmail.com>
2017-03-14 10:42:11 +0300

    Переписан класс simplex_realization. Теперь построение условий внутри класса.

--- lib/simplex_realization.py
+++ lib/simplex_realization.py
@@ -1,7 +1,56 @@
-from .simplex import Simplex
+from lib.simplex import Simplex
 
 
-class SimplesRealization(Simplex):
-    def __init__(self, num_vars, constraints, objective_function):
+class SimplexRealization(Simplex):
+    def __init__(self, num_vars, num_constaints, condition, function_coeffs, var_coeffs, signs, free_coeffs):
+        objective_function = SimplexRealization.build_objective(num_vars, condition, function_coeffs)
+        constraints = SimplexRealization.build_constraints(num_vars, num_constaints, var_coeffs, signs, free_coeffs)
 
-        super(SimplesRealization, self).__init__(num_vars, constraints, objective_function)
+        super(SimplexRealization, self).__init__(num_vars, constraints, objective_function)
+
+    @staticmethod
+    def build_objective(num_vars, condition, _coeffs):
+        _goal_func = ''
+
+        for _i in range(num_vars):
+            _goal_func += str(_coeffs[_i]) + 'x_{}'.format(_i+1) + ' + '
+        _goal_func = _goal_func[:-3:]
+
+        return condition, _goal_func
+
+    @staticmethod
+    def build_constraints(num_vars, constraints_count, constraint_coeffs, signs, free_coeffs):
+        _constraints = []
+
+        for _i in range(constraints_count):
+            _constraint = ''
+
+            for j in range(num_vars):
+                _constraint += str(constraint_coeffs[_i][j]) + 'x_{}'.format(j + 1) + ' + '
+
+            _constraint = _constraint[:-3:]
+            _constraint += ' ' + signs[_i] + ' ' + str(free_coeffs[_i])
+            _constraints.append(_constraint)
+
+        return _constraints
+
+
+if __name__ == "__main__":
+    n_vars = 4
+    n_count = 2
+    goal = "max"
+
+    function_coeffs = [2, 4, 4, 1]
+    vars_coeffs = [
+        [0, 1, 1, -1],
+        [2, 0, -2, 1],
+    ]
+
+    signs = ["=", "="]
+    free_coeffs = [1, 2]
+
+    Lp_system = SimplexRealization(n_vars, n_count, goal, function_coeffs, vars_coeffs, signs, free_coeffs)
+
+    print("\nResult: ")
+    print(Lp_system.solution)
+    print(Lp_system.optimize_val)
